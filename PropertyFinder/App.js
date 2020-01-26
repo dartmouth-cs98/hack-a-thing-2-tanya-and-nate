0fb2f3c2a29f89c20d0abe89728e15deff309fc9
tanya-shah <tanya.shah@dali.dartmouth.edu>
2020-01-26 17:49:30 -0500

    property finder not fully working

---
+++ PropertyFinder/App.js
@@ -0,0 +1,62 @@
+'use strict';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  NavigatorIOS,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import SearchPage from './SearchPage';
+
+class SearchPage extends Component<{}> {
+  render() {
+  return <Text style={styles.description}>Search for houses to buy!</Text>;
+}
+};
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+  },
+  container: {
+  flex: 1,
+},
+});
+
+export default App;
